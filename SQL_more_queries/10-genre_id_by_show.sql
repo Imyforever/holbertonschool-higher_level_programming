697187ab72fdf5eda8e173f7ceb5d1be20f97fdf
Imyforever <109327496+Imyforever@users.noreply.github.com>
2026-01-29 00:06:43 -0500

    Create 10-genre_id_by_show.sql

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,2 @@
+-- Genre by id shows
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv.shows.title ASC, tv_show_genres.genre_id ASC;
