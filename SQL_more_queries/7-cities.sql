5aa730a0bb0d21afb6bc5b08a98305ddd6de07c4
Imyforever <109327496+Imyforever@users.noreply.github.com>
2026-01-28 21:47:50 -0500

    Update 7-cities.sql

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,7 +1,9 @@
 -- Cities 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY_KEY UNIQUE,
+  id INT NOT NULL AUTO_INCREMENT,
   state_id INT NOT NULL,
   name VARCHAR(256) NOT NULL,
+  PRIMARY_KEY (id), 
+  UNIQUE (id),
   FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id));
